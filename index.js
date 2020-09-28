fbabec820448ed552446a62585265a509845cc06
Matheus <mateusanches2@gmail.com>
2020-09-28 20:40:30 -0300

    Fluxo de Requisições - Middleware

--- index.js
+++ index.js
@@ -2,8 +2,21 @@ const express = require('express');
 
 const app = express();
 
+const logMiddleware = (req, res, next) => {
+  console.log(
+    `HOST: ${req.headers.host},
+     URL: ${req.url},
+     METHOD: ${req.method}`,
+  );
+
+  req.appName = 'GoNode';
+  return next();
+};
+
+app.use(logMiddleware);
+
 app.get('/', (req, res) => {
-  return res.send(`Welcome, ${req.query.name}`);
+  return res.send(`Welcome to ${req.appName}, ${req.query.name}`);
 });
 
 app.get('/nome/:name', (req, res) => {
